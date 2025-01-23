fefbb4b4c88753f5386cb9bd8c7ec63358b35148
Katieln <139283838+Katieln@users.noreply.github.com>
2025-01-23 07:55:07 -0500

    hola

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { StudentsComponent } from './pages/students/students.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StudentsComponent,
+    HomeComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
